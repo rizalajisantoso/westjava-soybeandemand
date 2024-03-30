c2244177fb976528ad96e8d0595ea405b7e3113d
Rizal Aji Santoso <148060269+rizalajisantoso@users.noreply.github.com>
2024-03-30 20:45:37 +0700

    Update soybean query.sql

--- soybean query.sql
+++ soybean query.sql
@@ -1,11 +1,17 @@
-#schema name : kedelai
-#table name : production as produksi, land area as luas_panen
+-- schema name : kedelai
+-- table name : production as produksi, land area as luas_panen
 
-#view all data from both table
-select * from luas_panen;
-select * from produksi;
+-- view all data from both table
+select *
+  from luas_panen;
 
-#joining the two tables
-select luas_panen.*,produksi.produksi_kedelai
-from luas_panen inner join produksi
-on luas_panen.id = produksi.id
+select *
+  from produksi;
+
+-- joining the two tables
+select 
+  luas_panen.*,
+  produksi.produksi_kedelai
+
+from luas_panen 
+  inner join produksi on luas_panen.id = produksi.id
